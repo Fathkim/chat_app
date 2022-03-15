ef88bb74f710e709b1593b5b23528bdeb642ab14
fathkim <fathanhakim2006@gmail.com>
2022-03-15 14:06:35 +0700

    Chat App

--- lib/screens/registration_screens.dart
+++ lib/screens/registration_screens.dart
@@ -21,6 +21,7 @@ class _RegisterScreenState extends State<RegisterScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       backgroundColor: Colors.white,
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 24),
